62b994b7bbf1951fd5444ea85f67dfdea5b2ba4a
kimiya <kimiyamalekbala31@gmail.com>
2025-11-13 14:36:36 +0330

    add clean method

--- modules/parser_validator.py
+++ modules/parser_validator.py
@@ -4,3 +4,14 @@ def parser_validator(fasta :str):
          
         else:
             raise ValueError
+        
+
+
+def Clean(fasta):
+    lst = fasta.split("\n")
+    id_list = lst[0]
+    Sequence_list= lst[1]
+    ID_clean = clean_id(id_list)
+    Sequence_clean = clean_sequence(Sequence_list)
+    Dict = {"id": ID_clean , "sequence": Sequence_clean}
+    return Dict
